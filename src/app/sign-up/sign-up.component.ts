176b588e437b86df16886cdb9a9dd7eb0966e05b
RAGHAD <47270162+raghad6@users.noreply.github.com>
2021-01-31 09:09:58 +0200

    save data

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,26 +1,36 @@
 import { HttpClient  } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Validators, FormGroup, FormBuilder } from '@angular/forms';
-import { HttpEventType, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
-import { ValidationService } from '../services/validation.service';
-
+import { FormGroup, FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-  title = 'Forms';
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    name: new FormControl(''),
+    email: new FormControl(''),
+    gender: new FormControl(''),
+    avatar: new FormControl(''),
+    mobileNumber: new FormControl('')
+  });
 
-  constructor(  private fb: FormBuilder,
-    private validation : ValidationService,
-    private http: HttpClient) { }
+  
+  onSubmit() {
+    console.warn(this.profileForm.value);
+    
+  }
+
+  constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  
   
   
 }
